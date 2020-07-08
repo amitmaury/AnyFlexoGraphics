03af0a63f11a7eacd3dfb4fa6a1d13153cf7fe0d
Amit Maurya <amit.maurya@ltts.com>
2020-07-09 00:29:30 +0530

    changes in .env file and removed httpcreate server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
@@ -9,7 +11,12 @@ app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+
+app.listen(port, ()=>{
+    console.log("server is running on "+port)
+})
+
+// const server = http.createServer(app);
+
+// server.listen(port);
+// console.debug('Server listening on port ' + port);
